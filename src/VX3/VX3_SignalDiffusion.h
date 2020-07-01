88300c6b35f7e67915d17bff80d6b9d4c9a9281d
sida <sliu1@uvm.edu>
2020-07-01 16:46:17 -0400

    basic diffusion and test

--- src/VX3/VX3_SignalDiffusion.h
+++ src/VX3/VX3_SignalDiffusion.h
@@ -10,9 +10,17 @@
 class VX3_SignalDiffusion
 {
 public:
-    __device__ void doTimestep(); // diffusion
-    __device__ void addPartiles(double amount, VX3_Vec3D<> pos); // add signal particles to this environment
-    __device__ void removePartiles(double amount, VX3_Vec3D<> pos); // remove signal particles to this environment
+    __device__ void deviceInit(VX3_Vec3D<int> dim=VX3_Vec3D<int>(100,100,100));
+    __device__ void doTimestep(double dt); // diffusion
+    __device__ double quaryQuantityAtPosition(VX3_Vec3D<int> pos);
+    __device__ void addSolute(double amount, VX3_Vec3D<int> pos); // add signal particles to this environment
+    __device__ void removeSolute(double amount, VX3_Vec3D<int> pos); // remove signal particles to this environment
+
+    /* data */
+    double distance = 1e-2;
+    double diffusivity = 2e-9; // ionic in water. refer to: https://www.tandfonline.com/doi/pdf/10.1080/18811248.1996.9732037
+    VX3_Vec3D<int> dimension; // make the space into discrete subspace
+    double * map, * map_nextstep;
 };
 
 
