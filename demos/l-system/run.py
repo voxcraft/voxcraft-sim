33d9ffb8754bf079d63c425235bc83a68871fadb
Chris <cfusting@gmail.com>
2020-12-06 04:07:36 +0000

    Add pulsing voxels.

--- demos/l-system/run.py
+++ demos/l-system/run.py
@@ -6,9 +6,11 @@ import argparse
 parser = argparse.ArgumentParser(
     description="Generate a VXD file for the voxcraft simulation."
 )
-parser.add_argument("--growth_iterations", type=int)
-parser.add_argument("--search_radius", type=int)
-parser.add_argument("--max_voxels", type=int)
+parser.add_argument("--growth-iterations", type=int)
+parser.add_argument("--search-radius", type=int)
+parser.add_argument("--max-voxels", type=int)
+parser.add_argument("--record-history", 
+                    action="store_true")
 
 args = parser.parse_args()
 
@@ -31,4 +33,5 @@ l_system.expand(growth_function)
 # Prepare creature for simulation.
 X = l_system.to_tensor()
 C = tensor_to_cdata(X)
-add_cdata_to_xml(C, X.shape[0], X.shape[1], X.shape[2], "data/robot.vxd")
+add_cdata_to_xml(C, X.shape[0], X.shape[1], X.shape[2], "data/robot.vxd",
+                 record_history=args.record_history)
