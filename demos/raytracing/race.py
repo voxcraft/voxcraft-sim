050172d8512fe401abb2a0083ccff942d41303a6
Sam Kriegman <sam.kriegman@uvm.edu>
2021-09-25 00:36:17 -0400

    col

--- demos/raytracing/race.py
+++ demos/raytracing/race.py
@@ -93,8 +93,7 @@ for patch in range(N_PATCHES):
     tmp_body[tmp_body > 1] = 2  # only two material types
 
 blob = make_one_shape_only(tmp_body == 2)
-blob = blob.astype(np.int8)
-body[blob>0] = blob
+body[blob] = blob.astype(np.int8)
 
 # carve out random holes
 for cut in range(N_CUTS):
