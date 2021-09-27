e6489e0b88aeda8d376ed55173dfb5cf48fc52f8
Sam <skriegman@gmail.com>
2021-09-27 13:10:14 -0400

    light off for race demo

---
+++ demos/raytracing/submit.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Specify a partition
+#SBATCH --partition=dggpu
+# Request nodes
+#SBATCH --nodes=1
+# Request some processor cores
+#SBATCH --ntasks=4
+# Request GPUs
+#SBATCH --gres=gpu:1
+# Request memory
+#SBATCH --mem=32G
+# Run for X hours
+#SBATCH --time=1:00:00
+# Name of this job
+#SBATCH --job-name=X4
+# Output of this job, stderr and stdout are joined by default
+# %x=job-name %j=jobid
+#SBATCH --output=%x_%j.out
+#
+# change to the directory where you submitted this script
+cd ${SLURM_SUBMIT_DIR}
+#
+# your job execution follows:
+echo "Starting sbatch script myscript.sh at:`date`"
+# echo some slurm variables for fun
+echo "  running host:    ${SLURMD_NODENAME}"
+echo "  assigned nodes:  ${SLURM_JOB_NODELIST}"
+echo "  jobid:           ${SLURM_JOBID}"
+# show me my assigned GPU number(s):
+echo "  GPU(s):          ${CUDA_VISIBLE_DEVICES}"
+
+python3 race.py $1 $2 $3 $4
